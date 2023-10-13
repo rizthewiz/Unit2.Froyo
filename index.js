4803b67c12a785e9b3454fc806d616eaff7ece87
rizthewiz <rizthewiz>
2023-10-12 20:29:22 -0400

    finished assignment according to rulebrick. used objects

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
 const userInput = prompt(
-  "Please enter a list of comma-separated froyo flavors.",
-  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  "Please enter a list of comma-separated froyo flavors."
 );
 
-const splitFlavs = userInput.split(",");
+const order = {};
+const flavors = userInput.split(",");
 
-const array = [];
-for (let i = 0; i < splitFlavs.length; i++) {
-  array.push(splitFlavs[i]);
+//loop through flavors and add to order
+//add flavor to order
+//set value to 1
+// if flavor there raise value by 1
+for (let i = 0; i < flavors.length; i++) {
+  const flavor = flavors[i];
+  if (flavor in order) {
+    order[flavor]++;
+  } else {
+    order[flavor] = 1;
+  }
 }
 
-const arrToObj = (array) => {
-  const order = {};
-  for (let i = 0; i < array.length; i++) {
-    order[i] = array[i];
-    console.log(order);
-  }
-  return order;
-};
-
-// const listOrder = (array) => {
-//   let vanillaCount = 0;
-//   let strawberryCount = 0;
-//   let coffeCount = 0;
-//   for (const count in flavor) {
-//     if (array.length[i] == "vanilla") {
-//       vanillaCount += 1;
-//     } else if (array.length[i] == "strawberry") {
-//       strawberryCount += 1;
-//     } else if (array.length[i] == "coffee") {
-//       coffeCount += 1;
-//     }
-//   }
-//   return console.log(
-//     `You ordered: ${vanillaCount} vanilla\n ${strawberryCount} strawberry\n ${coffeCount} coffee`
-//   );
-// };
-console.log(array);
-// listOrder(array);
+console.log(order);
